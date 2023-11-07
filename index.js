f3efb56e10640f28ff4f97c1de9ada59bbfa9ac9
aamamun24 <bdmamaun100@gmail.com>
2023-11-07 16:48:38 +0600

    food post on the database

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.7exoi4x.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7exoi4x.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +26,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const foodCollection = client.db('foodBondDB').collection('foods')
+
+        app.post('/foods', async (req, res) => {
+            const newFood = req.body;
+            const result = await foodCollection.insertOne(newFood)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");

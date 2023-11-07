01021efcee518699d05d1e9d40da2213e55adcfc
aamamun24 <bdmamaun100@gmail.com>
2023-11-07 16:56:47 +0600

    get the food from database

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ async function run() {
 
         const foodCollection = client.db('foodBondDB').collection('foods')
 
+        app.get('/foods', async (req, res) => {
+            const cursor = foodCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
         app.post('/foods', async (req, res) => {
             const newFood = req.body;
             const result = await foodCollection.insertOne(newFood)
